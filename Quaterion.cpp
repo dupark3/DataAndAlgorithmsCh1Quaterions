c37dab9ab667a1faafdae9a1ad847c55b2159174
DuParkLinux <dupark3@gmail.com>
2018-04-20 12:33:52 -0400

    Fixed memory leak by returning by value from arithmetic operators instead of by reference

--- Quaterion.cpp
+++ Quaterion.cpp
@@ -5,34 +5,34 @@
 
 using namespace std;
 
-Quaterion& operator+(const Quaterion& q1, const Quaterion& q2){
+Quaterion operator+(const Quaterion& q1, const Quaterion& q2){
     double a = q1.a + q2.a;
     double b = q1.b + q2.b;
     double c = q1.c + q2.c;
     double d = q1.d + q2.d;
-    Quaterion* q_ptr = new Quaterion(a, b, c, d);
-    return *q_ptr;
+    Quaterion new_quaterion(a, b, c, d);
+    return new_quaterion;
 }
 
-Quaterion& operator-(const Quaterion& q1, const Quaterion& q2){
+Quaterion operator-(const Quaterion& q1, const Quaterion& q2){
     double a = q1.a - q2.a;
     double b = q1.b - q2.b;
     double c = q1.c - q2.c;
     double d = q1.d - q2.d;
-    Quaterion* q_ptr = new Quaterion(a, b, c, d);
-    return *q_ptr;
+    Quaterion new_quaterion(a, b, c, d);
+    return new_quaterion;
 }
 
-Quaterion& operator*(const Quaterion& q1, const Quaterion& q2){
+Quaterion operator*(const Quaterion& q1, const Quaterion& q2){
     double a = (q1.a * q2.a) - (q1.b * q2.b) - (q1.c * q2.c) - (q1.d * q2.d);
     double b = (q1.a * q2.b) + (q1.b * q2.a) + (q1.c * q2.d) - (q1.d * q2.c);
     double c = (q1.a * q2.c) + (q1.c * q2.a) + (q1.d * q2.b) - (q1.b * q2.d);
     double d = (q1.a * q2.d) + (q1.d * q2.a) + (q1.b * q2.c) - (q1.c * q2.b);
-    Quaterion* q_ptr = new Quaterion(a, b, c, d);
-    return *q_ptr;
+    Quaterion new_quaterion(a, b, c, d);
+    return new_quaterion;
 }
 
-Quaterion& operator/(const Quaterion& q1, const Quaterion& q2){
+Quaterion operator/(const Quaterion& q1, const Quaterion& q2){
     double a = ((q1.a * q2.a) + (q1.b * q2.b) + (q1.c * q2.c) + (q1.d * q2.d)) / 
                ((q2.a * q2.a) + (q2.b * q2.b) + (q2.c * q2.c) + (q2.d * q2.d));
     double b = ((q1.b * q2.a) - (q1.a * q2.b) - (q1.d * q2.c) + (q1.c * q2.d)) /
@@ -41,8 +41,8 @@ Quaterion& operator/(const Quaterion& q1, const Quaterion& q2){
                ((q2.a * q2.a) + (q2.b * q2.b) + (q2.c * q2.c) + (q2.d * q2.d));
     double d = ((q1.d * q2.a) - (q1.c * q2.b) + (q1.b * q2.c) - (q1.a * q2.d)) /
                ((q2.a * q2.a) + (q2.b * q2.b) + (q2.c * q2.c) + (q2.d * q2.d));
-    Quaterion* q_ptr = new Quaterion(a, b, c, d);
-    return *q_ptr;
+    Quaterion new_quaterion(a, b, c, d);
+    return new_quaterion;
 }
 
 std::ostream& operator<<(std::ostream& os, const Quaterion& q){
--- Quaterion.h
+++ Quaterion.h
@@ -8,7 +8,7 @@ public:
     Quaterion() { }
     Quaterion(double p, double q, double r, double s) : a(p), b(q), c(r), d(s) { }
     ~Quaterion() { }
-    
+
     void print(std::ostream& os) { os << a << " + " << b << "i + " << c << "j + " << d << "k\n"; }
 
 private:
@@ -17,19 +17,19 @@ private:
     double c;
     double d;
 
-    friend Quaterion& operator+(const Quaterion&, const Quaterion&);
-    friend Quaterion& operator-(const Quaterion&, const Quaterion&);
-    friend Quaterion& operator*(const Quaterion&, const Quaterion&);
-    friend Quaterion& operator/(const Quaterion&, const Quaterion&);
+    friend Quaterion operator+(const Quaterion&, const Quaterion&);
+    friend Quaterion operator-(const Quaterion&, const Quaterion&);
+    friend Quaterion operator*(const Quaterion&, const Quaterion&);
+    friend Quaterion operator/(const Quaterion&, const Quaterion&);
 
     friend std::ostream& operator<<(std::ostream&, const Quaterion&);
     friend std::istream& operator>>(std::istream&, Quaterion&);
 };
 
-Quaterion& operator+(const Quaterion&, const Quaterion&);
-Quaterion& operator-(const Quaterion&, const Quaterion&);
-Quaterion& operator*(const Quaterion&, const Quaterion&);
-Quaterion& operator/(const Quaterion&, const Quaterion&);
+Quaterion operator+(const Quaterion&, const Quaterion&);
+Quaterion operator-(const Quaterion&, const Quaterion&);
+Quaterion operator*(const Quaterion&, const Quaterion&);
+Quaterion operator/(const Quaterion&, const Quaterion&);
 
 std::ostream& operator<<(std::ostream&, const Quaterion&);
 std::istream& operator>>(std::istream&, Quaterion&);
